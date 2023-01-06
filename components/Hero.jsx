1c53bb84dce877a732de9371ea9765d75a753757
Riccardo Benevelli <riccardobenevelli@MBP-di-Riccardo.station>
2023-01-06 12:33:36 +0100

    Foto da mobile

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,24 +7,24 @@ import Img2 from '../public/blob.svg'
 
 const Hero = () => {
   return (
-    <div className="h-screen bg-gradient-to-tl from-orange-300 to-orange-100">
-        <div className='max-w-[1350px] m-auto'>
+    <div className="h-screen bg-gradient-to-tl from-orange-400 to-orange-50">
+        <div className='max-w-[1350px] m-auto h-screen overflow-hidden'>
 
         <div>
-            <h1 className='text-center text-4xl xl:text-7xl text-slate-800 font-bold p-5 pt-32 xl:pb-20 xl:pt-40 '>
+            <h1 className='text-center text-4xl xl:text-7xl text-slate-800 font-bold p-5 pt-24 xl:pb-20 xl:pt-40 '>
                 Lo spazio di dibattito di Reggio Emilia
             </h1>
         </div>
         {/* contenitore testo e immagine */}
-        <div className='xl:flex xl:flex-row text-slate-900'>
-            <div className='xl:w-1/2'>
+        <div className='md:flex ml:flex-row text-slate-900'>
+            <div className='md:w-1/2'>
                 <h2 className='font-semibold text-2xl xl:text-4xl text-center p-5 xl:p-10 leading-relaxed'>Da giovani, <br /> per giovani.</h2>
 
-                <p className='text-xl font-medium px-8 xl:px-10 text-justify leading-relaxed'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, tenetur quos tempora soluta facilis fuga inventore! Recusandae rem optio, voluptas laboriosam exercitationem numquam, esse itaque molestias hic, ipsam consequatur ullam.</p>
+                <p className='text-xl font-medium px-8 xl:px-10 text-justify '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, tenetur quos tempora soluta facilis fuga inventore!</p>
             </div>
-            <div className=' w-1/2 relative'>
-                <Image src={Img1} alt="foto di un evento di caffeina politica" className='w-4/6 rounded-xl absolute z-20'/>
-                <Image src={Img2} alt="bolla grafica" className='w-[150%] absolute top-[-20%] right-0 z-10'/>
+            <div className=' md:w-1/2 relative p-10'>
+                <Image src={Img1} alt="foto di un evento di caffeina politica" className='w-2/3 md:w-4/6 left-8 rounded-xl absolute z-20 drop-shadow-[0_35px_35px_rgba(0,0,0,0.50)]'/>
+                <Image src={Img2} alt="bolla grafica" className='w-[150%] absolute top-[-20%] left-16 z-10'/>
             </div>
         </div>
         
