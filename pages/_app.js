57f5380acff36b66d7fb2bd7ccd878d66dda8936
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-02-08 23:11:39 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
-import NavBar from '../components/NavBar'
-import Grid from '../components/Grid'
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }) {
   return (
   <>
+    <GoogleAnalytics trackPageViews strategy="lazyOnload"/>
     <Layout>
       <Component {...pageProps} />
     </Layout>
