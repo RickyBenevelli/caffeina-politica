d88ab97d1bde02abd8c46fa4b5bf75cbf8ecf536
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-05-01 14:45:40 +0200

    cambiato evento di default nella sezione calendario

--- components/EventContext.jsx
+++ components/EventContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const EventContext = createContext("");
 
 const EventContextProvider = ({children}) => {
-    const [selected, setSelected] = useState(2)
+  const [selected, setSelected] = useState(3)
 
   return (
     <EventContext.Provider value={{selected, setSelected}}>{children}</EventContext.Provider>
