7c98f070f7786fe23f5c04b5360a98c83f05a486
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-05-11 13:56:28 +0200

    finito di aggiungere l'articolo sulle carceri in Italia

--- components/Bibliography.jsx
+++ components/Bibliography.jsx
@@ -10,11 +10,11 @@ const Bibliography = ({ children }) => {
   };
   return (
     <div className="w-full bg-slate-500 rounded-md p-2 shadow-lg">
-      <div className=" text-slate-50 flex flex-row items-center justify-start text-sm" onClick={handleShow}>
+      <div className=" text-slate-50 flex flex-row items-center justify-start text-sm cursor-pointer" onClick={handleShow}>
         <HiArrowSmUp size={20} className={`${show? "" : "rotate-180"} transition-transform duration-500`}/>
         <span className="ml-2">Bibliography</span>
       </div>
-        <div className={`text-slate-100 text-xs lg:text-sm ${show? "" : "hidden"} transition-all duration-1000`}>
+        <div className={`text-slate-100 text-xs lg:text-sm ${show? "" : "hidden"} transition-all duration-1000 list-decimal`}>
           <hr className=" my-2" />
           {children}
         </div>
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'upload.wikimedia.org', 'www.avvenire.it', 'e3.365dm.com', 'pnrr.protezionecivile.it', 'openbdap.rgs.mef.gov.it', 'unsplash.com', 'www.rapportoantigone.it', 'www.garantedetenutilazio.it'],
+    domains: ['images.unsplash.com', 'upload.wikimedia.org', 'www.avvenire.it', 'e3.365dm.com', 'pnrr.protezionecivile.it', 'openbdap.rgs.mef.gov.it', 'unsplash.com', 'www.rapportoantigone.it', 'www.garantedetenutilazio.it', 'asvis.it'],
   },
 }
 
