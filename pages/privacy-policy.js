1fbcfff2bbb55c3f637f162213181d81c72b93b3
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-05-25 18:45:07 +0200

    aggiunta prima parte di gladio

---
+++ pages/privacy-policy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <section className={`bg-gradient-to-br from-[#dc6041] via-[#e6a24f] to-[#dc6041]`}>
+
+        <div className='flex flex-col pt-20 pb-14 text-slate-900 px-5 max-w-[1350px] m-auto items-center justify-center'>
+
+        </div>
+    </section>
+  )
+}
+
+export default Privacy
