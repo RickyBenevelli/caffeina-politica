0313cb18bbe5dd94985b7ced102b36c554a3af4b
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-08-04 18:30:58 +0200

    aggiunto articolo: il problema dell'obiezione di coscienza

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'upload.wikimedia.org', 'www.avvenire.it', 'e3.365dm.com', 'pnrr.protezionecivile.it', 'openbdap.rgs.mef.gov.it', 'unsplash.com', 'www.rapportoantigone.it', 'www.garantedetenutilazio.it', 'asvis.it', 'plrchile.com', 'www.repstatic.it', 'www.triesteallnews.it', 'cheesecake.articleassets.meaww.com', 'st.ilfattoquotidiano.it', 'www.historialudens.it', 'live.staticflickr.com', 'www.avvenire.it', 'media.famigliacristiana.it', 'img-prod.tgcom24.mediaset.it', 'www.lavocedellevoci.it', 'www.riminiduepuntozero.it', 'media.tenor.com', 'www.intoscana.it', 'imgcdn.agendadigitale.eu', 'www.repstatic.it', 'cdn.lindipendente.online'],
+    domains: ['images.unsplash.com', 'upload.wikimedia.org', 'www.avvenire.it', 'e3.365dm.com', 'pnrr.protezionecivile.it', 'openbdap.rgs.mef.gov.it', 'unsplash.com', 'www.rapportoantigone.it', 'www.garantedetenutilazio.it', 'asvis.it', 'plrchile.com', 'www.repstatic.it', 'www.triesteallnews.it', 'cheesecake.articleassets.meaww.com', 'st.ilfattoquotidiano.it', 'www.historialudens.it', 'live.staticflickr.com', 'www.avvenire.it', 'media.famigliacristiana.it', 'img-prod.tgcom24.mediaset.it', 'www.lavocedellevoci.it', 'www.riminiduepuntozero.it', 'media.tenor.com', 'www.intoscana.it', 'imgcdn.agendadigitale.eu', 'www.repstatic.it', 'cdn.lindipendente.online', 'www.cittanuova.it', 'i0.wp.com'],
   },
 }
 
